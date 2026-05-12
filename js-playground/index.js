e3109ba3ad58b333c281c7fb7e9f660438d23cf6
mozumasu <ouri1229@gmail.com>
2026-05-12 12:55:31 +0900

    refactor: Refactor task functions to use arrow syntax and improve clarity

--- js-playground/index.js
+++ js-playground/index.js
@@ -3,12 +3,11 @@ let id = 1
 // tasks [ {id: 1, title, isComplete }, {id: 2, title, isComplete }]
 
 // 入力: String 処理: Arrayに追加
-function addTask(title) {
-  // task {id, title: title, isComplete: false}
-  // id 初回は1, 呼び出されるたびにインクリメントされる
-  let incrementableId = id++
+const addTask = (title) => {
+  const incrementableId = id++
   const task = { id: incrementableId, title: title, isComplete: false }
   tasks.push(task)
+
   console.log(`追加したにょ
 id: ${incrementableId}
 タイトル: ${title}
@@ -17,9 +16,9 @@ id: ${incrementableId}
 
 // タスクを表示
 // 入力: Array 出力: Arrayを表示
-function listTask() {
-  console.log(`
-<タスクリスト>`)
+const listTask = () => {
+  console.log(`<タスクリスト>
+`)
   for (const task of tasks) {
     const mark = task.isComplete ? "✔" : " "
     console.log(`[${mark}]: ${task.title}`)
@@ -27,21 +26,15 @@ function listTask() {
 }
 
 // タスクを完了
-// 入力:Array 出力: 完了したタスクと未完了のタスクの表示
-// 処理:
-// select * from tasks where id = うけとったid
-// update(みつけたtaskのisCompleteフラグをtrueにする)
-function completeTask(id) {
-  const targetTask = tasks.find((t) => {
-    return t.id == id
-  })
-
+// 入力:Array 出力: 完了したタスクの表示
+const completeTask = (id) => {
+  const targetTask = tasks.find((t) => t.id == id)
   targetTask.isComplete = true
 
   const mark = targetTask.isComplete ? "✔" : " "
-
   console.log("タスクを完了したにょ")
-  console.log(`[${mark}]: targetTask.title`)
+  console.log(`[${mark}]: ${targetTask.title}`)
+
 }
 
 // データ構造
@@ -49,6 +42,7 @@ function completeTask(id) {
 // id: 検索
 // title: 表示
 // isComplete: 完了してるかどうか
+
 addTask("初めてのタスク")
 addTask("2こめのタスク")
 listTask()
